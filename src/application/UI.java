ba382c2fd48da505e6ea70ce6eff09ddb92aa39e
Leonardo Mundim <leosmundim@yahoo.com.br>
2021-07-21 17:31:26 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,12 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() { 
+		System.out.print("\033[H\033[2J"); 
+		System.out.flush(); 
+	} 
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -38,7 +44,7 @@ public class UI {
 			return new ChessPosition(column, row);
 		}
 		catch (RuntimeException e) {
-			throw new InputMismatchException("Erro lendo posição de xadrez. Valores válidos de a1 a h8.");
+			throw new InputMismatchException("Erro lendo posicao de xadrez. Valores validos de a1 a h8.");
 		}
 	}
 
