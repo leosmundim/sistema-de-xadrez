af39f7eb79c9c265a8e6ac73e2294e6e22c6eb4e
Leonardo Mundim <leosmundim@yahoo.com.br>
2021-07-28 15:26:36 -0300

    Handling captured pieces

---
+++ src/teste/Testes.java
@@ -0,0 +1,22 @@
+package teste;
+
+import application.UI;
+import boardgame.Board;
+import chess.ChessMatch;
+import chess.ChessPiece;
+
+public class Testes {
+
+	public static void main(String[] args) {
+
+		
+		ChessPiece[][] mat = new ChessPiece[8][8];
+		
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printMatch(chessMatch);
+		
+
+	}
+
+}
