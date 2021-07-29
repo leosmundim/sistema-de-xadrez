677e8989d15db23641eaa8d296e785c715e3a5ad
Leonardo Mundim <leosmundim@yahoo.com.br>
2021-07-29 18:55:19 -0300

    Knight

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -10,6 +10,11 @@ public class Pawn extends ChessPiece {
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
@@ -71,11 +76,6 @@ public class Pawn extends ChessPiece {
 		}
 
 		return mat;
-	}
-	
-	@Override
-	public String toString() {
-		return "P";
 	}
 
 }
